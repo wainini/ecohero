9b828281fea27e4049ccec3f92f7265b6b4229f4
croiscer <derianbello15@gmail.com>
2024-08-04 13:35:58 +0700

    add new input package

---
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    public Vector2 MovementInput { get; private set; }
+    public bool InteractInput { get; private set; }
+
+    public void SetMovementInput(InputAction.CallbackContext ctx)
+    {
+        MovementInput = ctx.ReadValue<Vector2>();
+    }
+
+    public void SetInteractInput(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            InteractInput = true;
+        }
+        else if (ctx.canceled)
+        {
+            InteractInput = false;  
+        }
+    }
+}
